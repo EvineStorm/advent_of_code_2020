8c3d2f0c69bde7783fdae67b224580a42c72e62a
evine <evinestorm@gmail.com>
2021-11-22 14:49:33 +0100

    answered question day8 b

--- src/day8/HandheldHalting.java
+++ src/day8/HandheldHalting.java
@@ -6,30 +6,51 @@ package day8;
 
 // Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp).
 // What is the value of the accumulator after the program terminates?
-// (answer)
+// 2304
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HandheldHalting {
+
 
     public static void main(String[] args) throws IOException {
         new HandheldHalting().run();
     }
 
     private void run() throws IOException {
+        List<String> originalInstructions = Files.readAllLines(Path.of("src/day8/input_day8.txt"));
+        for (int i = 0; i < originalInstructions.size(); i++) {
+            String instruction = originalInstructions.get(i);
+            String instructionType = instruction.split(" ", 2)[0];
+            List<String> editedInstructions = new ArrayList<>(originalInstructions);
+
+            if (instructionType.equals("jmp")) {
+                instruction = "nop" + instruction.substring(3);
+                editedInstructions.set(i, instruction);
+            } else if (instructionType.equals("nop")) {
+                instruction = "jmp" + instruction.substring(3);
+                editedInstructions.set(i, instruction);
+            }
+
+            int answer = runInstructions(editedInstructions);
+            if (answer != 0) {
+                System.out.println(answer);
+                break;
+            }
+        }
+    }
+
+    private int runInstructions(List<String> instructions) {
         int accumulator = 0;
-        List<String> instructions = Files.readAllLines(Path.of("src/day8/input_day8.txt"));
         for (int i = 0; i < instructions.size(); i++) {
             String line = instructions.get(i);
             String[] operationArgument = line.split(" ", 2);
 
-            if (operationArgument[0].equals("")) {
-                System.out.println(accumulator);
-                break;
-            }
+            if (operationArgument[0].equals("")) return 0;
 
             if (operationArgument[0].equals("acc")) {
                 instructions.set(i, "");
@@ -37,10 +58,11 @@ public class HandheldHalting {
             } else if (operationArgument[0].equals("jmp")) {
                 instructions.set(i, "");
                 i = (doOperation(i, operationArgument)) - 1;
-            } else if (operationArgument[0].equals("nop")) {
-                continue;
             }
+
+            if (i == (instructions.size() - 1)) return accumulator;
         }
+        return 0;
     }
 
     private int doOperation(int j, String[] operationArgument) {
